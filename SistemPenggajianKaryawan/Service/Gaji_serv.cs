d958db502c9bd4f076f5ded90755e3adfd6d3ee3
Pratama Putra <theperfectpratama.stu@pnc.ac.id>
2026-05-24 19:25:09 +0700

    Add Service

---
+++ SistemPenggajianKaryawan/Service/Gaji_serv.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemPenggajianKaryawan.Service
+{
+    internal class Gaji_serv
+    {
+    }
+}
